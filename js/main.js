9933b386baab2c4d3b1dcb1025e06c73e705e785
nguyendev2018 <nguyendev2018@gmail.con>
2022-05-04 22:04:24 +0700

    --- Auto-Git Commit ---

--- js/main.js
+++ js/main.js
@@ -1,6 +1,17 @@
 const getElement = (id) => {
     return document.getElementById(id)
 }
-const clickTurn = getElement("currentTurn").addEventListener("click", (() => {
-    console.log('ds');
-}))
+const clickTurn = getElement("currentTurn").addEventListener("click", function () {
+    if (this.classList[0] == "cross") {
+        this.classList.remove("cross");
+        this.classList.add("circle");
+    }
+    else {
+        this.classList.add("cross");
+        this.classList.remove("circle");
+    }
+    // if (this.classList[0] === "circles") {
+    //     this.classList.add("cross");
+    //     this.classList.remove("circles");
+    // }
+})
