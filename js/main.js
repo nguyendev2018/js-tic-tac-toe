c3955ae24b6344d501a74a13a179389cc6dc5185
nguyendev2018 <nguyendev2018@gmail.con>
2022-05-04 23:04:36 +0700

    --- Auto-Git Commit ---

--- js/main.js
+++ js/main.js
@@ -1,23 +1,35 @@
 const getElement = (id) => {
     return document.getElementById(id)
 }
-const clickTurn = getElement("currentTurn").addEventListener("click", function () {
-    if (this.classList[0] == "cross") {
-        this.classList.remove("cross");
-        this.classList.add("circle");
+
+const renderTable = function () {
+    const listTable = document.querySelectorAll('#cellList li');
+
+    for (let i = 0; i < listTable.length; i++) {
+        listTable[i].addEventListener("click", function () {
+            if (listTable[i].textContent == "") {
+                const changeTurn = getElement("currentTurn")
+                if (changeTurn.classList[0] == "cross") {
+                    changeTurn.classList.remove("cross");
+                    changeTurn.classList.add("circle");
+                    const content = "x";
+                    listTable[i].innerHTML = content;
+
+                }
+                else if (changeTurn.classList[0] == "circle") {
+                    changeTurn.classList.add("cross");
+                    changeTurn.classList.remove("circle");
+                    const content = "o";
+                    listTable[i].innerHTML = content;
+                }
+            }
+            else {
+                return
+            }
+
+
+        })
+
     }
-    else {
-        this.classList.add("cross");
-        this.classList.remove("circle");
-    }
-
-})
-const listTable = document.querySelectorAll('#cellList li');
-for (let i = 0; i < listTable.length; i++) {
-    listTable[i].addEventListener("click", function () {
-        if (this.classList == "") {
-
-        }
-    })
-
 }
+renderTable()
