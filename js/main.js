4cd11a15c685bb5e7c47cad4a5946872ee6d45e2
nguyendev2018 <nguyendev2018@gmail.con>
2022-05-05 10:26:35 +0700

    first commit

--- js/main.js
+++ js/main.js
@@ -1,7 +1,5 @@
 import { getCellElementList } from "./selector.js";
-console.log(getCellElementList);
 // Global variables
 let currentTurn = "cross";
 let isGameEnded = false;
 let cellValues = new Array(9).fill("");
-console.log(cellValues);
